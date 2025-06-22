9ca727502e975f70140f205b4f39706568d9be71
savatoxa <savatoxa@yandex.ru>
2025-06-22 05:24:11 +0300

    set instead of list

--- src/DivideTriangles.kt
+++ src/DivideTriangles.kt
@@ -1,24 +1,22 @@
 object DivideTriangles {
-    fun divideTriangle (triangle: Triangle) : Triangle{
+    private fun divideTriangle (triangle: Triangle) : Triangle{
         val divide = fun(point1: Point2D, point2: Point2D): Point2D = Point2D((point1.x + point2.x)/2, (point1.y + point2.y)/2)
         return Triangle(divide(triangle.A,triangle.B), divide(triangle.B,triangle.C), divide(triangle.C,triangle.A))
     }
-    fun addMissingTriangles(triangle: Triangle) : MutableList<Triangle>{
+    private fun addMissingTriangles(triangle: Triangle) : MutableSet<Triangle>{
         val newTriangle = divideTriangle(triangle)
         val newTriangleLeft = Triangle(newTriangle.A, triangle.B, newTriangle.B)
         val newTriangleRight = Triangle(newTriangle.C, newTriangle.B, triangle.C)
         val newTriangleUpper = Triangle(triangle.A, newTriangle.A, newTriangle.C)
-        return mutableListOf(newTriangle, newTriangleLeft, newTriangleRight, newTriangleUpper)
+        return mutableSetOf(newTriangle, newTriangleLeft, newTriangleRight, newTriangleUpper)
     }
-    fun divideTriangles (triangles: MutableList<Triangle>, startIdx: Int = 0) :  MutableList<Triangle>{
-        if(triangles.count() < numTriangles) {
-            val endIdx = triangles.size
-            for (i in startIdx until endIdx) {
-                triangles.addAll(addMissingTriangles(triangles[i]))
-            }
-            return divideTriangles(triangles, startIdx = endIdx)
-        }
-        else
+    fun divideTriangles(triangles: MutableSet<Triangle>) : MutableSet<Triangle> {
+        if (triangles.size >= numTriangles)
             return triangles
+        val newTriangles = mutableSetOf<Triangle>()
+        for (triangle in triangles)
+            newTriangles.addAll(addMissingTriangles(triangle))
+        triangles.addAll(newTriangles)
+        return divideTriangles(triangles)
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -2,14 +2,12 @@ import java.io.File
 const val fileName = "triangle_between_points.html"
 const val width = 600
 const val height = 500
-const val numTriangles = 100
+const val numTriangles = 200
 
 fun main() {
-    triangleABC.printTriangle()
-
     val canvas = DrawTriangle.drawCanvas(width, height)
     File(fileName).writeText(canvas)
-    val resTriangles = DivideTriangles.divideTriangles(mutableListOf(triangleABC))
+    val resTriangles = DivideTriangles.divideTriangles(mutableSetOf(triangleABC))
     for (triangle in resTriangles)
         DrawTriangle.drawTriangle(triangle, fileName)
 }
